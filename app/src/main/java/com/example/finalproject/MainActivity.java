b8ea8817395f2cf64c87683afe1c89ecbe1d48e0
aladah <aladah.moore@gmail.com>
2022-03-12 18:12:36 -0500

    toolbar

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.finalproject.databinding.ActivityMainBinding;
+import com.example.finalproject.databinding.ActivityNewsBinding;
+
+public class MainActivity extends DrawerBaseActivity {
+    //variable to hold bindings
+    ActivityMainBinding activityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //inflate the common navbar
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        allocateActivityTitle("Main");
+        setContentView(activityMainBinding.getRoot());
+
     }
 }
