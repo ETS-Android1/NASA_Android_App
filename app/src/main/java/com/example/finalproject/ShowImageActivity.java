2de20985a9d3852aefa2f0983eaaa779cc0d9c46
Breakwood-15 <61804501+Breakwood-15@users.noreply.github.com>
2022-04-13 17:55:36 -0400

    Remove debug logs for date information

--- app/src/main/java/com/example/finalproject/ShowImageActivity.java
+++ app/src/main/java/com/example/finalproject/ShowImageActivity.java
@@ -110,13 +110,10 @@ public class ShowImageActivity extends DrawerBaseActivity {
         //Receive date from DatePicker in previous Activity
         Intent receivedDate = getIntent();
         datePassed = receivedDate.getStringExtra("Date");
-        Log.d("DatePickerPassedDate", datePassed);
 
         //Convert date formatting for display
         LocalDate date = LocalDate.parse(datePassed, DateTimeFormatter.ofPattern("yyyy-M-d"));
         formattedDate = date.format(DateTimeFormatter.ofPattern("MMMM d, uuuu", Locale.CANADA));
-        Log.d("Date", String.valueOf(date));
-        Log.d("Date", String.valueOf(formattedDate));
 
         //Initialize progress bar, set progress and visibility
         pb = findViewById(R.id.progressBar);
